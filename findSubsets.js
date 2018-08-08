5b6b6023b564ca872a6d8d5efcfbae6487858880
Nikhil Arora <narora200@gmail.com>
2018-08-08 15:56:32 +0530

    [Feat]: Exhaustive search implemented recursively

--- findSubsets.js
+++ findSubsets.js
@@ -1,15 +1,9 @@
 // findSubsets([1,3,4,5,6], 10) => [[1,4,5], [1,3,6], [4,6]]
 
 const findSubsets = (arr, total) => {
-  const subsets = [];
-  const customFindSubsets = ([head, ...tail]) => {
-    let sum = head;
-    tail.forEach(num => {
-      sum += num;
+  const exhaustiveSearch = ([...head, tail]) => {
 
-      if (sum === total) {
-        subsets.push()
-      }
-    })
-  };
+  }
 };
+
+// knapsack problem
--- knapsack.js
+++ knapsack.js
@@ -6,27 +6,84 @@
 
 // Solution: 220
 
-const max = (a, b) => (((a - b) > 0) ? a : b);
+const max = (a, b, arr) => {
+  console.log(arr);
+  if (a.info > b.info) {
+    const bIndex = arr.indexOf(b.headValue);
+    if (bIndex !== -1) {
+      arr.splice(bIndex, 1);
+    }
 
-const knapsackAlgo = (remainingWeight, [headValue, ...tailValue], [headWeight, ...tailWeight]) => {
-  if (remainingWeight === 0) {
-    return 0;
-  }
-  if ((headValue === undefined) && (headWeight === undefined)) {
-    return 0;
-  }
+    if (a.headValue === -1) {
+      arr.push(a.headValue);
+    }
 
-  if (headWeight > remainingWeight) {
-    return knapsackAlgo(remainingWeight, tailValue, tailWeight);
-  }
+    return {
+      result: a.info,
+    }
+  } else {
+    const aIndex = arr.indexOf(a.headValue);
+    if (aIndex !== -1) {
+      arr.splice(aIndex, 1);
+    }
 
-  return max(knapsackAlgo(remainingWeight, tailValue, tailWeight),
-      headValue + knapsackAlgo((remainingWeight - headWeight), tailValue, tailWeight))
+    if (b.headValue === -1) {
+      arr.push(b.headValue);
+    }
+    return {
+      data: b.headValue,
+      result: b.info,
+    }
+  }
+}
+
+
+const functionWrapper = () => {
+  let finalArr = [];
+  const knapsackAlgo = (remainingWeight, [headValue, ...tailValue], [headWeight, ...tailWeight]) => {
+    if (remainingWeight === 0) {
+      return 0;
+    }
+    if ((headValue === undefined) && (headWeight === undefined)) {
+      return 0;
+    }
+
+    if (headWeight > remainingWeight) {
+      return knapsackAlgo(remainingWeight, tailValue, tailWeight);
+    }
+    return max(
+      {
+        key: tailWeight,
+        value: tailValue,
+        info: knapsackAlgo(remainingWeight, tailValue, tailWeight),
+        headValue: undefined,
+      },
+      {
+        key: tailWeight,
+        value: tailValue,
+        info: headValue + knapsackAlgo((remainingWeight - headWeight),tailValue, tailWeight),
+        headValue: headValue,
+      }, finalArr).result;
+  };
+
+  knapsackAlgo(50, [60, 100, 120], [10, 20, 30]);
+  console.log(finalArr);
+
+  // knapsackAlgo(50, [60, 100, 120], [60, 70, 380]);
+  // console.log(finalArr);
+
+  // knapsackAlgo(70, [60, 179, 120], [10, 70, 60]);
+  // console.log(finalArr);
+
+  // knapsackAlgo(70, [60, 181, 120], [10, 70, 60]);
+  // console.log(finalArr);
+
+  // knapsackAlgo(70, [60, 180, 120], [10, 70, 60]);
+  // console.log(finalArr);
+
+  return;
 };
 
 
-console.log(knapsackAlgo(50, [60, 100, 120], [10, 20, 30]));
 
-console.log(knapsackAlgo(70, [60, 179, 120], [10, 70, 60]));
-console.log(knapsackAlgo(70, [60, 181, 120], [10, 70, 60]));
-console.log(knapsackAlgo(70, [60, 180, 120], [10, 70, 60]));
+console.log(functionWrapper());
