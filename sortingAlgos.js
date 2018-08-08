bed16cfd0fb08c8fe836f6e64442ed1a16150d2a
Nikhil Arora <narora200@gmail.com>
2018-08-08 22:36:24 +0530

    [Feat]: Bubble Sort done

--- sortingAlgos.js
+++ sortingAlgos.js
@@ -1,5 +1,16 @@
+const array = [1, 9, 8, 3, 6, 7, -1];
+
+
+const swap = (arr, outerLoopIndex, innerLoopIndex) => {
+  const temp = arr[outerLoopIndex];
+  arr[outerLoopIndex] = arr[innerLoopIndex];
+  arr[innerLoopIndex] = temp;
+};
+
+
+/* SELECTION SORT */
 const selectionSort = (arr) => {
-  for (let outerLoopIndex = 0; outerLoopIndex < arr.length; outerLoopIndex += 1) {
+for (let outerLoopIndex = 0; outerLoopIndex < arr.length; outerLoopIndex += 1) {
     let min = arr[outerLoopIndex];
     let index = outerLoopIndex;
     for (let innerLoopIndex = (outerLoopIndex + 1); innerLoopIndex < arr.length; innerLoopIndex += 1) {
@@ -8,14 +19,29 @@ const selectionSort = (arr) => {
         index = innerLoopIndex;
       }
     }
-    const temp = arr[outerLoopIndex];
-    arr[outerLoopIndex] = arr[index];
-    arr[index] = temp;
+    swap(arr, outerLoopIndex, index);
   }
   return arr;
 };
 
-const array = [1, 9, 8, 3, 6, 7, -1];
-
 const selectionSortResult = selectionSort(array);
 console.log('Selection Sort Result: ', selectionSortResult);
+
+
+/* BUBBLE SORT */
+const bubbleSort = (arr) => {
+  for (let outerLoopIndex = 0; outerLoopIndex < (arr.length - 1); outerLoopIndex += 1) {
+    for (let innerLoopIndex = (outerLoopIndex + 1); innerLoopIndex < (arr.length - outerLoopIndex - 1); innerLoopIndex += 1) {
+      const element = arr[outerLoopIndex];
+      if (element > arr[innerLoopIndex]) {
+        swap(arr, outerLoopIndex, innerLoopIndex);
+      }
+    }
+  }
+
+  return arr;
+};
+
+const bubbleSortResult = bubbleSort(array);
+console.log('Bubble Sort Result: ', bubbleSortResult);
+
